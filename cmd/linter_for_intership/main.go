d58e7a9994bcb672502e61cd2da40183dee59ffb
hhheys <dmitriy.tsyganckov@gmail.com>
2026-02-13 23:42:26 +0300

    Created golangci-lint plugin

--- cmd/linter_for_intership/main.go
+++ cmd/linter_for_intership/main.go
@@ -1,5 +1,18 @@
 package linterforintership
 
-func main() {
+import (
+	logcheck "intershiplinter/log_check"
 
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+)
+
+func main() {
+	analyzers := []*analysis.Analyzer{
+		logcheck.LogEnglishLetterAnalyzer,
+		logcheck.LogLowercaseAnalyzer,
+		logcheck.LogSensitiveDataAnalyzer,
+		logcheck.LogSpecialSymbolAnalyzer,
+	}
+	multichecker.Main(analyzers...)
 }
